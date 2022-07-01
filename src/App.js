530436f13f1bff0a3fa3df8aab83864d4c235ab4
hannahyvonne16 <58607653+hannahyvonne16@users.noreply.github.com>
2022-06-30 19:18:52 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+import ColorBlock from './ColorBlock';
+import ColorForm from './ColorForm.js';
+
+function App() {
+  let [colors, setColors] = useState(['violet','blue','lightblue', 'green', 'greenyellow', 'orange', 'red'])
+  let colorMap = colors.map((color, i) => {
+      return (
+          <div>
+              <ColorBlock key={i} color={color} />
+          </div>      
+      )}
+  )
+  
+  const addColor = (newColor) => {
+    setColors([...colors, newColor])
+  }
+
+  return (
+    <div className="App">
+      {colorMap}
+      <ColorForm addColor={addColor}/>
+    </div>
+  );
+}
+
+export default App;
